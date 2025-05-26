6a167eb6807a1c1ab552a99394cfa86e21a0967f
tr3vochka-rmz <kanalmoveron@gmail.com>
2025-05-26 23:51:01 +0700

    f3i332e

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
     baseURL:
         import.meta.env.VITE_API_URL ||
-        'booking-task-backend.railway.internal/api',
+        'booking-task-backend-production.up.railway.app/api',
 });
 
 api.interceptors.request.use((config) => {
