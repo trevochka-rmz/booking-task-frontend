d4851caade1209ada97d101e7fbb81448831a6bb
tr3vochka-rmz <kanalmoveron@gmail.com>
2025-05-26 23:09:59 +0700

    f3i3

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
     baseURL:
         import.meta.env.VITE_API_URL ||
-        'https://booking-task-backend.onrender.com',
+        'https://booking-task-backend.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
